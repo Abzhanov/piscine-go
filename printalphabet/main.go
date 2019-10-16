909b42ea663676f120ec65b5228e3b16ce5da6be
Abzhanov <abzhanov@github.com>
2019-10-16 17:16:39 +0600

    q2 print

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,11 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main () {
-
-	var alphabet string = "abcdefghijklmnopqrstuvwxyz\n"
-
-	for i := 'a'; i<= 'z'; i++ {
-		z01.PrintRune(rune) alphabet[i]))\
+		for i := 'a'; i<= 'z'; i++ {
+		z01.PrintRune(i)
 	}
-
+	z01.PrintRune(10)
 }
