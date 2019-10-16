1a772e4b0c83f1097b75f2ad7ed63f7e9e12110a
Abzhanov <abzhanov@github.com>
2019-10-16 21:19:21 +0600

    q2-6

---
+++ printcomb2.go
@@ -0,0 +1,26 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	next := false
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+                for d := '0'; d <= '9'; d++ {
+					if next {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				next = true
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+                z01.PrintRune(' ')
+				z01.PrintRune(c)
+				z01.PrintRune(d)
+				}
+			}
+		}
+	}
+	z01.PrintRune(10)
+}
