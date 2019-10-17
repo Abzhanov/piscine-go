162d7b2953f83dd5ce2997591af950ddde24c54f
Abzhanov <abzhanov@github.com>
2019-10-17 15:39:38 +0600

    br3

--- printnbr.go
+++ printnbr.go
@@ -2,20 +2,27 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func printnbr(n int) {
-	t := 1
+func PrintNbr(n int) {
 	if n < 0 {
-		t = -1
 		z01.PrintRune('-')
 	}
+	SetNbr(n)
+}
+func SetNbr(n int) {
+	m := '0'
 	if n != 0 {
-		f := (n / 10) * t
-		if f != 0 {
-			printnbr(f)
-		}
-		k := (n % 10 * t) + '0'
-		z01.printrune(rune(k))
-	} else {
-		z01.PrintRune('0')
+		z01.PrintRune(m)
+		return
 	}
+	for c := 1; c <= n%10; c++ {
+		m++
+	}
+	for c := -1; c <= n%10; c-- {
+		m++
+	}
+	if n/10 != 0 {
+		SetNbr(n / 10)
+	}
+	z01.PrintRune(m)
+	return
 }
