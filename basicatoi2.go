51aee5a8df69ef7e171d0aba2f4bb6221a2a2678
Abzhanov <52921430+Abzhanov@users.noreply.github.com>
2019-10-19 15:27:11 +0600

    Update basicatoi2.go

--- basicatoi2.go
+++ basicatoi2.go
@@ -2,16 +2,19 @@ package piscine
 
 func BasicAtoi2(s string) int {
 	x := 0
-	for _, y := range s {
-		if y > '9' && y < '0' {
-			return 0
-		} else {
-		z := 0 
-		for i := '1'; i <= y; i++ {
-			z++
-		}
-		x = x*10 + z
-		}	
-	}		
+	for _, y := range []rune(s) {
+	if y <= '9' && y >= '0' {
+		i := '0'
+		j := 0
+	for ; i != y; i++ {
+		j++
+	}
+	if x != 0 || j != 0 {
+		x = x*10 + j
+	}
+	} else {
+		return 0
+	}
+	}
 	return x
 }
