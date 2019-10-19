9e95f1c07d42642764817507f6b9f83dcfd11b39
Abzhanov <abzhanov@github.com>
2019-10-19 19:01:41 +0600

    q3-11-1

---
+++ atoi.go
@@ -0,0 +1,50 @@
+package piscine
+
+func Atoi(str string) int {
+	//strReversed := StrRev(str)
+	x := 0
+	dm := false
+	if StrLen(str) != 0 {
+		if rune(str[0]) == '-' {
+			for _, y := range []rune(str) {
+				if y == '-' {
+					if dm {
+						return 0
+					}
+					dm = true
+					continue
+				} else if y <= '9' && y >= '0' {
+					d := '0'
+					j := 0
+					for ; d != y; d++ {
+						j--
+					}
+					if x != 0 || j != 0 {
+						x = x*10 + j
+					}
+				} else {
+					return 0
+				}
+			}
+			return x
+		} else {
+			for _, y := range []rune(str) {
+				if y <= '9' && y >= '0' {
+					d := '0'
+					j := 0
+					for ; d != y; d++ {
+						j++
+					}
+					if x != 0 || j != 0 {
+						x = x*10 + j
+					}
+				} else {
+					return 0
+				}
+			}
+			return x
+		}
+	} else {
+		return 0
+	}
+}
