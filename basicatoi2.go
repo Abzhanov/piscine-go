193e78fbf0ed611405117d84b6c38cc2e08014f9
Abzhanov <52921430+Abzhanov@users.noreply.github.com>
2019-10-19 15:37:36 +0600

    Add files via upload

---
+++ basicatoi2.go
@@ -0,0 +1,20 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	x := 0
+	for _, y := range []rune(s) {
+		if y <= '9' && y >= '0' {
+			i := '0'
+			j := 0
+			for ; i != y; i++ {
+				j++
+			}
+			if x != 0 || j != 0 {
+				x = x*10 + j
+			}
+		} else {
+				return 0
+		}
+	}
+	return x
+}
