d3d946439bae3a5d981b980e2921bc80832f8d23
Abzhanov <abzhanov@github.com>
2019-10-21 17:04:39 +0600

    q4-2-2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,10 +4,10 @@ func RecursiveFactorial(nb int) int {
 	a := 0
 	if nb < 0 || nb > 12 {
 		return 0
-	} else  if nb == 0 {
+	} else if nb == 0 {
 		return 1
 	} else {
-			a = nb * RecursiveFactorial(nb-1)
-	}	
+		a = nb * RecursiveFactorial(nb-1)
+	}
 	return a
 }
