cbebd6a8394ba1ff5abe0a439c9b1401b57334fc
Abzhanov <abzhanov@github.com>
2019-10-21 19:48:55 +0600

    q4-4-2

--- recursivepower.go
+++ recursivepower.go
@@ -7,9 +7,6 @@ func RecursivePower(nb int, power int) int {
 	}
 	if power == 0 {
 		return 1
-	} else if power%2 == 0 {
-		a = RecursivePower(nb, power/2)
-		a *= a
 	} else {
 		a = nb * RecursivePower(nb, power-1)
 	}
