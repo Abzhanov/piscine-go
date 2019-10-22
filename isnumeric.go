038e6e58437c8b4c3570e941f7e57dfbdfe498f8
Abzhanov <abzhanov@github.com>
2019-10-23 00:58:46 +0600

    q5-12

---
+++ isnumeric.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsNumeric(str string) bool {
+	x := []rune(str)
+	l := StrLen(str)
+	for i := 0; i <= l-1; i++ {
+		if (x[i] >= 0) && (x[i] <= 47) || (x[i] >= 58) && (x[i] <= 64) || (x[i] >= 91) && (x[i] <= 96) || (x[i] >= 123) && (x[i] <= 127) {
+			return false
+		}
+	}
+	return true
+}
