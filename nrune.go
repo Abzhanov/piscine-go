5b40ec13254c8497604894b47b07561e214f8a60
Abzhanov <abzhanov@github.com>
2019-10-22 21:07:34 +0600

    q5-5-6

--- nrune.go
+++ nrune.go
@@ -3,8 +3,8 @@ package piscine
 func NRune(s string, n int) rune {
 	array := []rune(s)
 	for i := 0; i <= n; i++ {
-		if i >= n && n > 0 {
-			return (array[i-1])
+		if i == n {
+			return array[n-1]
 		}
 	}
 	return 0
