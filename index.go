349ac7af578a25c6ed8a55233babab3064a16fbe
Abzhanov <abzhanov@github.com>
2019-10-22 22:56:12 +0600

    q5-7-1

--- index.go
+++ index.go
@@ -1,12 +1,16 @@
 package piscine
 
 func Index(s string, toFind string) int {
-
 	str := []rune(s)
 	c := []rune(toFind)
-	n := len(str)
-	m := len(c)
-
+	n := 0
+	m := 0
+	for range str {
+		n++
+	}
+	for range c {
+		m++
+	}
 	for i := 0; i <= n-m; i++ {
 		if toFind == s[i:i+m] {
 			return (i)
