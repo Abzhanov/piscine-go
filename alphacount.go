cd08eabbc1ebb97b7c417fa0abbf1f61e9dd89b8
Abzhanov <abzhanov@github.com>
2019-10-22 17:01:35 +0600

    q5-1-2

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,13 @@
 package piscine
 
 func AlphaCount(str string) int {
-		a := []rune(str)
-		counter := 0
-		for _, i := range a {
-				if 'a' <= i && i <= 'z' || 'A' <= i && i <= 'Z' {
-						counter++
-				}
-		}
-		return counter
-}	
+	a := []rune(str)
+	counter := 0
+	for _, i := range a {
+			if 'a' <= i && i <= 'z' || 'A' <= i && i <= 'Z' {
+					counter++
+			}
+	}
+	return counter
+}
+	
