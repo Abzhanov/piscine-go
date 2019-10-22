02b425666c2bbdfe70047cd1f9d71315c0152bcb
Abzhanov <abzhanov@github.com>
2019-10-23 00:21:48 +0600

    q5-11

---
+++ capitalize.go
@@ -0,0 +1,24 @@
+package piscine
+
+func Capitalize(s string) string {
+	l := len(s)
+	x := []rune(s)
+	n := true
+	for i := 0; i < l; i++ {
+		if n && (x[i] >= 'a' && x[i] <= 'z') {
+			x[i] = x[i] - 32
+			n = false
+		} else if n && (x[i] >= 'A' && x[i] <= 'Z') {
+			n = false
+		} else if n == false && (x[i] >= 'a' && x[i] <= 'z') {
+
+		} else if n == false && (x[i] >= 'A' && x[i] <= 'Z') {
+			x[i] = x[i] + 32
+		} else if (x[i] >= '0' && x[i] <= '9') {
+			n = false
+		} else {
+			n = true
+		}
+	}
+	return string(x)
+}
