1c112f2c0f2fb4d839b75e33990e9ec2e599ffab
Abzhanov <abzhanov@github.com>
2019-10-22 17:03:09 +0600

    q5-1-3

--- alphacount.go
+++ alphacount.go
@@ -4,10 +4,9 @@ func AlphaCount(str string) int {
 	a := []rune(str)
 	counter := 0
 	for _, i := range a {
-			if 'a' <= i && i <= 'z' || 'A' <= i && i <= 'Z' {
-					counter++
-			}
+		if 'a' <= i && i <= 'z' || 'A' <= i && i <= 'Z' {
+				counter++
+		}
 	}
 	return counter
 }
-	
