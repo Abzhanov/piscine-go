b95a5ca7afafc5b5f7cd0b035a6922ab3a3a1a9b
Abzhanov <abzhanov@github.com>
2019-10-23 01:25:44 +0600

    q5-15-1

--- isprintable.go
+++ isprintable.go
@@ -4,7 +4,7 @@ func IsPrintable(str string) bool {
 	x := []rune(str)
 	l := StrLen(str)
 	for i := 0; i <= l-1; i++ {
-		if (x[i] >= 32) && (x[i] <= 127) {
+		if (x[i] >= 0) && (x[i] <= 31) {
 			return false
 		}
 	}
