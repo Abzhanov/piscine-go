559c6b786ddb46120fdfb2cbc80d2984c927eddc
Abzhanov <abzhanov@github.com>
2019-10-23 01:18:21 +0600

    q5-13-1

--- islower.go
+++ islower.go
@@ -4,7 +4,7 @@ func IsLower(str string) bool {
 	x := []rune(str)
 	l := StrLen(str)
 	for i := 0; i <= l-1; i++ {
-		if (x[i] >= 0) && (x[i] <= 96) || (x[i] >= 123) && (x[i] <= 127){
+		if (x[i] >= 0) && (x[i] <= 96) || (x[i] >= 123) && (x[i] <= 127) {
 			return false
 		}
 	}
