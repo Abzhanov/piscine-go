762caa769db03e86bf09426b53d8fba781f1aa7d
Abzhanov <abzhanov@github.com>
2019-10-22 20:36:38 +0600

    q5-3-2

--- trimatoi.go
+++ trimatoi.go
@@ -9,22 +9,22 @@ func TrimAtoi(s string) int {
 			j++
 		}
 	}
-	last := 0
+	n := 0
 	for i := 0; i < j; i++ {
-		if x[i] == '+' && last == 0 {
-			last = 1
-		} else if x[i] == '-' && last == 0 {
-			last = 2
-		} else if x[i] >= 48 && s[i] <= 57 && (last == 0 || last == 3) {
+		if x[i] == '+' && n == 0 {
+			n = 1
+		} else if x[i] == '-' && n == 0 {
+			n = 2
+		} else if x[i] >= 48 && s[i] <= 57 && (n == 0 || n == 3) {
 			res = res*10 + (int(x[i]) - 48)
-			last = 3
-		} else if x[i] >= 48 && s[i] <= 57 && last == 1 {
+			n = 3
+		} else if x[i] >= 48 && s[i] <= 57 && n == 1 {
 			res = res*10 + (int(x[i]) - 48)
-		} else if x[i] >= 48 && s[i] <= 57 && last == 2 {
+		} else if x[i] >= 48 && s[i] <= 57 && n == 2 {
 			res = res*10 + (int(x[i]) - 48)
 		}
 	}
-	if last == 1 || last == 3 {
+	if n == 1 || n == 3 {
 		return res
 	}
 	return res * (-1)
