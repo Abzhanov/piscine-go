c56b603dffc95af928f427ca3d5f504dc04e016a
Abzhanov <abzhanov@github.com>
2019-10-23 00:56:33 +0600

    q5-12-8

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,8 @@ package piscine
 
 func IsAlpha(str string) bool {
 	x := []rune(str)
-	for i := 0; i <= len(x)-1; i++ {
+	l := StrLen(str)
+	for i := 0; i <= l-1; i++ {
 		if (x[i] >= 0) && (x[i] <= 47) || (x[i] >= 58) && (x[i] <= 64) || (x[i] >= 91) && (x[i] <= 96) || (x[i] >= 123) && (x[i] <= 127) {
 			return false
 		}
