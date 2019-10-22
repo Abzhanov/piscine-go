c80b7a16efa0b34da27403ff4841152acacbbf9b
Abzhanov <abzhanov@github.com>
2019-10-23 01:17:17 +0600

    q5-13

---
+++ islower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsLower(str string) bool {
+	x := []rune(str)
+	l := StrLen(str)
+	for i := 0; i <= l-1; i++ {
+		if (x[i] >= 0) && (x[i] <= 96) || (x[i] >= 123) && (x[i] <= 127){
+			return false
+		}
+	}
+	return true
+}
