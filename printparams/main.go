b053e2a072d6bb68ec0e608ab1ef33c77cabc22b
Abzhanov <abzhanov@github.com>
2019-10-23 18:07:17 +0600

    q6-2-2

--- printparams/main.go
+++ printparams/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	piscine ".."
+	"github.com/01-edu/z01"
 	"os"
 )
+
+func PrintStr(str string) {
+	for _, h := range str {
+		z01.PrintRune(h)
+	}
+}
 
 func main() {
 	x := os.Args
 	for i, y := range x {
 		if i != 0 {
-			piscine.PrintStr(y)
+			PrintStr(y)
 		}
 	}
 }
