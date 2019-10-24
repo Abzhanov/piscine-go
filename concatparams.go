505f1e42e45c1996c0f84a1819052da43d9a9a37
Abzhanov <abzhanov@github.com>
2019-10-24 17:36:44 +0600

    q7-3

---
+++ concatparams.go
@@ -0,0 +1,20 @@
+package piscine
+
+func l(d []string) int {
+	m := 0
+	for _, _ = range d {
+		m++
+	}
+	return m
+}
+
+func ConcatParams(args []string) string {
+	res := ""
+	for i, j := range args {
+		res += j
+		if i != l(args)-1 {
+			res += "\n"
+		}
+	}
+	return res
+}
